e35ff572b93062016242d92419c6b5947e4184c0
andrewjharris96 <85307621+ajharris96@users.noreply.github.com>
2021-06-12 22:54:46 +0000

    Prompted user for name and greeted them. Asked one question and provided feedback

--- candidate-testing.js
+++ candidate-testing.js
@@ -5,43 +5,57 @@ const input = require('readline-sync');
 // TODO 1.1a: Define candidateName // 
 let candidateName;
 // TODO 1.2a: Define question, correctAnswer, and candidateAnswer //
-let question;
-let correctAnswer;
-let candidateAnswer;
+let question = "Who was the first American woman in space?";
+let correctAnswer = "Sally Ride";
+let candidateAnswer ="";
 let questions;
 let correctAnswers;
 let candidateAnswers;
 
 
-function askForName() {
+function askForName(){candidateName = input.question("What is your name?");
+
   // TODO 1.1b: Ask for candidate's name //
 
 }
 
-function askQuestion() {
+
+
+function askQuestion() {candidateAnswer = input.question(question);
   // TODO 1.2b: Ask candidate the question and assign the response as candidateAnswer //
 
 
-}
 
+}
 function gradeQuiz(candidateAnswers) {
 
   // TODO 1.2c: Let the candidate know if they have answered the question correctly or incorrectly // 
+if (candidateAnswer === correctAnswer){
 
+  let grade = "Passed";
 
-  let grade;
+  console.log("You " + grade)
   
 
   return grade;
+}else{
+  let grade = "Failed";
+  console.log("You " + grade);
+  return grade;
 }
-
+}
 function runProgram() {
   askForName();
   // TODO 1.1c: Ask for candidate's name //
-  
+  console.log("Hello, " + candidateName);
   askQuestion();
   gradeQuiz(this.candidateAnswers);
+
+
+
 }
+
+
 
 // Don't write any code below this line //
 // And don't change these or your program will not run as expected //
