09f52af57154136a37874163f49ef1eb56fac7c8
andrewjharris96 <85307621+ajharris96@users.noreply.github.com>
2021-06-17 15:08:09 +0000

    multiple questions and basic feedback

--- candidate-testing.js
+++ candidate-testing.js
@@ -1,17 +1,19 @@
 const input = require('readline-sync');
 let candidateName;
-let question = "Who was the first American woman in space? ";
-let correctAnswer = "Sally Ride";
-let candidateAnswer;
-let questions;
-let correctAnswers;
-let candidateAnswers;
+let question
+let correctAnswer
+let candidateAnswer
+let questions = ["Who was the first American woman in space? ", "True or false: 5 kilometer == 5000 meters? ", "(5 + 3)/2 * 10 = ? ", "Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2? ", "What is the minimum crew size for the ISS? "]
+let correctAnswers = ["Sally Ride",	"true", "40", "Trajectory", "3"]
+let candidateAnswers = [''];
 let grade;
 function askForName(){
   candidateName = input.question("What is your name? ");
 }
+let x = 0
 function askQuestion(){
-  candidateAnswer = input.question(question);
+  candidateAnswers[x] = input.question(questions[x]);
+  x = x+1
 }
 function gradeQuiz(candidateAnswers){ 
 if (candidateAnswer === correctAnswer){
@@ -25,10 +27,19 @@ if (candidateAnswer === correctAnswer){
 function runProgram(){
   askForName();
   console.log("Hello, " + candidateName);
-  askQuestion();
-  grade = gradeQuiz(this.candidateAnswers);
-  console.log("You " + grade);
-}
+  for (let i = 0; i < (questions.length); i++){
+    askQuestion();
+  }
+  //grade = gradeQuiz(this.candidateAnswers);
+  //console.log("You " + grade);
+console.log(`Candidate Name: ${candidateName}\n`);
+for (let a = 0; a<5;a++){
+    console.log(`${a+1}\)${questions[a]}\nYour Answer: ${candidateAnswers[a]}\nCorrect Answer: ${correctAnswers[a]}\n`);
+  }
+
+  }
+
+
 
 
 
