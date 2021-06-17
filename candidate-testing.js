3343b77a69351306665d293bd8dbcc0e2da3d38b
andrewjharris96 <85307621+ajharris96@users.noreply.github.com>
2021-06-17 17:08:05 +0000

    fixing2

--- candidate-testing.js
+++ candidate-testing.js
@@ -6,15 +6,18 @@ let candidateAnswer = " ";
 let score = 0
 let questions = ["Who was the first American woman in space? ", "True or false: 5 kilometer == 5000 meters? ", "(5 + 3)/2 * 10 = ? ", "Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2? ", "What is the minimum crew size for the ISS? "]
 let correctAnswers = ["Sally Ride",	"true", "40", "Trajectory", "3"]
-let candidateAnswers = [''];
+let candidateAnswers = ['']; 
+
 function askForName(){
   candidateName = input.question("What is your name? ");
 }
+
 let x = 0
 function askQuestion(){
   candidateAnswers[x] = input.question(questions[x]);
   x = x+1
 }
+
 function gradeQuiz(ans){ 
 for (let b=0;b<5;b++){
 if (ans[b].toLowerCase() === correctAnswers[b].toLowerCase()){
@@ -24,10 +27,11 @@ if (ans[b].toLowerCase() === correctAnswers[b].toLowerCase()){
 grade = (score/5)*100;
 return grade
 }
+
 function runProgram(){
   askForName();
   console.log("Hello, " + candidateName);
-  for (let i = 0; i < (questions.length); i++){
+  for (let i = 0; i < (5); i++){
     askQuestion();
   }
 console.clear();
@@ -35,7 +39,7 @@ console.log(`Candidate Name: ${candidateName}\n`);
 for (let a = 0; a<5;a++){
     console.log(`${a+1}\)${questions[a]}\nYour Answer: ${candidateAnswers[a]}\nCorrect Answer: ${correctAnswers[a]}\n`);
   }
-grade = gradeQuiz(this.candidateAnswers);
+grade = gradeQuiz(candidateAnswers);
 console.log(`>>> Overall Grade: ${grade}% (${score} of 5 responses correct) <<<`);
 if (grade>=80){
   console.log(">>> Status: PASSED <<<");
@@ -43,6 +47,7 @@ if (grade>=80){
   console.log(">>> Status: FAILED <<<");
   }
   }
+
 // Don't write any code below this line //
 // And don't change these or your program will not run as expected //
 module.exports = {
