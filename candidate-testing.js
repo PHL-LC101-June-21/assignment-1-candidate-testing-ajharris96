a97e5de29b2682040218f640a29255a7466ad1f4
andrewjharris96 <85307621+ajharris96@users.noreply.github.com>
2021-06-17 16:03:47 +0000

    trying to fix so it'll pass tests

--- candidate-testing.js
+++ candidate-testing.js
@@ -1,7 +1,7 @@
 const input = require('readline-sync');
 let candidateName;
-let question
-let correctAnswer
+let question = "Who was the first American woman in space? "
+let correctAnswer = "Sally Ride"
 let candidateAnswer
 let grade = 0
 let questions = ["Who was the first American woman in space? ", "True or false: 5 kilometer == 5000 meters? ", "(5 + 3)/2 * 10 = ? ", "Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2? ", "What is the minimum crew size for the ISS? "]
